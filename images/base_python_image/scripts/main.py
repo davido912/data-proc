5d25c8f6cb32418330c76c27d0a3bba88ab4f453
David Ohayon <david.ohayon@raisin.com>
2021-02-19 01:21:55 +0100

    added a cli-lookalike interaction

--- images/base_python_image/scripts/main.py
+++ images/base_python_image/scripts/main.py
@@ -1,6 +1,7 @@
 import sys
 from file_op import import_source
-
+import pyfiglet
+from termcolor import colored
 from pathlib import Path
 from os.path import join
 
@@ -9,14 +10,25 @@ ROOT_DIR = Path(__file__).parent.absolute()
 TABLE_METADATA_DIR = join(ROOT_DIR, "table_metadata")
 
 
-def main(date_filter_val=None):
-    import_source(tables_md_dir=TABLE_METADATA_DIR, date_filter_val=date_filter_val)
+def main():
+    print(colored(pyfiglet.figlet_format("WELCOME"), "green"))
+    print(
+        """Choose one of the following: \n
+    (1) load all events
+    (2) load specific date from events
+    """
+    )
+    while True:
+        choice = input("Your choice: ")
+        if choice in ["1", "2"]:
+            break
+        print("Invalid selection, please choose again")
+    if choice == "1":
+        import_source(tables_md_dir=TABLE_METADATA_DIR)
+    else:
+        date_filter_val = input("Please insert date in YYYY-MM-DD format: ")
+        import_source(tables_md_dir=TABLE_METADATA_DIR, date_filter_val=date_filter_val)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(date_filter_val=sys.argv[1])
-    elif len(sys.argv) == 1:
-        main()
-    else:
-        raise ValueError
+    main()
