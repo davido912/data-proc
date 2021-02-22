d68c896153e064e850941fef768fff092b41bb2a
David Ohayon <david.ohayon@raisin.com>
2021-02-22 15:27:02 +0100

    run and tmux shells

--- images/base_python_image/in_container/tmux.sh
+++ images/base_python_image/in_container/tmux.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+COMMAND=$1
+
 bash -c 'while !</dev/tcp/postgres-db/5432; do sleep 2; done;'
-tmux new-session -d -s python-app;
-tmux send-keys -t python-app.0 'tmux split-window' ENTER
-tmux send-keys -t python-app.0 'python /project/scripts/main.py' ENTER
-sleep 3 # sometimes tmux requires a bit of pause
-tmux send-keys -t python-app.1 'psql postgres://dev:password@postgres-db:5432/events' ENTER
-tmux a -t python-app
+
+if [[ $COMMAND == "rabbitmq" ]]; then
+  tmux new-session -d -s rabbitmq-app
+  tmux send-keys -t rabbitmq-app.0 'tmux split-window' ENTER
+  tmux send-keys -t rabbitmq-app.0 'python /project/scripts/queue_implementation/cli.py' ENTER
+  sleep 3 # sometimes tmux requires a bit of pause
+  tmux send-keys -t rabbitmq-app.1 'psql postgres://dev:password@postgres-db:5432/events' ENTER
+  tmux a -t rabbitmq-app
+
+else
+  tmux new-session -d -s python-app
+  tmux send-keys -t python-app.0 'tmux split-window' ENTER
+  tmux send-keys -t python-app.0 'python /project/scripts/main.py' ENTER
+  sleep 3 # sometimes tmux requires a bit of pause
+  tmux send-keys -t python-app.1 'psql postgres://dev:password@postgres-db:5432/events' ENTER
+  tmux a -t python-app
+
+fi
--- run.sh
+++ run.sh
@@ -1,26 +1,54 @@
-#!/usr/bin/env bash
-
-COMMAND=$1
+#!/bin/bash
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-if [[ $COMMAND == "deploy" ]]; then
-  docker-compose -f docker-compose.yaml -f docker-compose.tests.yaml up -d
-  TESTS_EXIT_CODE=$(docker wait tests)
+function Usage() {
+  printf "\t${RED} Invalid choice${NC}
+	\tUsage: ./$(basename $0) --deploy/--destroy OR --deploy-rabbitmq/--destroy-rabbitmq \n "
+}
+
+if [ "$#" -ne 1 ]; then
+  printf ${RED}'ERROR! You must provide only one argument!\n'${NC} >&2
+  exit 1
+fi
+
+COMMAND=$1
+
+function evalTests() {
+  local TESTS_EXIT_CODE=${1}
   docker logs tests
 
   if [ -z ${TESTS_EXIT_CODE+x} ] || [ "$TESTS_EXIT_CODE" -ne 0 ]; then
     printf "${RED}Tests Failed${NC} - Exit Code: $TESTS_EXIT_CODE\n"
+    exit 1
   else
     printf "${GREEN}Tests Passed${NC}\n"
-    sleep 4 # the sleep here is just to have a few seconds to glance at the logs printed
-    docker exec -it python-app /bin/bash /tmux.sh
   fi
-elif [[ $COMMAND == "destroy" ]]; then
+}
+
+if [[ $COMMAND == "--deploy" ]]; then
+  docker-compose -f docker-compose.yaml -f docker-compose.tests.yaml up -d
+  TESTS_EXIT_CODE=$(docker wait tests)
+  evalTests TESTS_EXIT_CODE
+  sleep 4 # the sleep here is just to have a few seconds to glance at the logs printed
+  docker exec -it python-app /bin/bash /tmux.sh
+
+elif [[ $COMMAND == "--deploy-rabbitmq" ]]; then
+  docker-compose -f docker-compose-rabbitmq.yaml -f docker-compose-rabbitmq.tests.yaml up -d
+  TESTS_EXIT_CODE=$(docker wait tests)
+  evalTests TESTS_EXIT_CODE
+  sleep 4 # the sleep here is just to have a few seconds to glance at the logs printed
+  docker exec -it cli /bin/bash /tmux.sh rabbitmq
+
+elif
+  [[ $COMMAND == "--destroy-rabbitmq" ]]
+then
+  docker-compose -f docker-compose-rabbitmq.yaml -f docker-compose-rabbitmq.tests.yaml down
+elif [[ $COMMAND == "--destroy" ]]; then
   docker-compose -f docker-compose.yaml -f docker-compose.tests.yaml down
 else
-  echo "Invalid command, available commands are deploy and destroy"
+  Usage
   exit 1
 fi
